09ba1ad6e6585683ee024af0a7ad0fe4b2019889
Antonella Coscarelli <coscarellianto@gmail.com>
2023-11-30 15:56:57 +0100

    - Fix errores - Add steps

--- cypress/e2e/my_first_test.cy.js
+++ cypress/e2e/my_first_test.cy.js
@@ -3,4 +3,6 @@
 it ('Iniciar Google', function(){
     cy.visit('https://www.google.com')
     cy.get('.spoKVd > :nth-child(1)').click()
+    cy.get('#APjFqb').type('Youtube{enter}')
+    cy.contains('Videos').click({ force: true }) 
 })
