5189a818bb9fe148f3a571221e6fc844fbada90d
Antonella Coscarelli <coscarellianto@gmail.com>
2023-11-30 18:59:34 +0100

    - Test Log in added

--- cypress/e2e/my_first_test.cy.js
+++ cypress/e2e/my_first_test.cy.js
@@ -6,3 +6,15 @@ it ('Iniciar Google', function(){
     cy.get('#APjFqb').type('Youtube{enter}')
     cy.contains('Videos').click({ force: true }) 
 })
+
+it.only('login test',function(){
+    cy.visit('https://practicetestautomation.com/practice-test-login/')
+    cy.contains('Test login')
+    cy.get('#username').type('student')
+    cy.get('#password').type('Password123')
+    cy.get('#submit').click()
+    cy.contains('Logged In Successfully')
+    cy.contains('Congratulations student. You successfully logged in!')
+    cy.contains('Log out').click()
+    cy.contains('This is a simple Login page. Students can use this page to practice writing simple positive and negative LogIn tests. Login functionality is something that most of the test automation engineers need to automate.')
+})
